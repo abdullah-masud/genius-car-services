41e69bb35b47a28a01c262e9e00b0b08869a7a7e
Abdullah Al Masud <masudctg00@gmail.com>
2022-04-14 19:44:18 +0600

    email authentication done

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
     return (
         <div>
             <h2>Service Detail {serviceId}</h2>
+            <Link to="/checkout">
+                <button className='btn btn-primary'>Proceed checkout</button>
+            </Link>
         </div>
     );
 };
